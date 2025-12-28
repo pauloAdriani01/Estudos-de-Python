1c6a27fd27e5e77dc96a676325fa23140a52e55e
Paulo Henrique S. Adriani <paulohadriani2007@gmail.com>
2025-12-28 13:28:16 -0300

    Commit 9 - Tuplas Finalizadas
    Atualizações:
    - Terminei o projetinho que estava fazendo para estudar tuplas
    
    Próximos Passos:
    - Começar a teoria de dicionários

--- Projetinho - Sistema de Gerenciamento de Registros de Alunos/main.py
+++ Projetinho - Sistema de Gerenciamento de Registros de Alunos/main.py
@@ -16,6 +16,16 @@ def adicionar_aluno(lista_alunos):
 
     ID_aluno = input("Digite o ID do aluno: ")
 
+    if not verificar_ID_aluno(lista_alunos, ID_aluno):
+        os.system('cls')
+
+        print(f"[ERRO]Já existe um aluno cadastrado com o ID {ID_aluno}!\n")
+
+        print("Voltando ao menu principal.")
+        os.system('pause')
+
+        return lista_alunos
+
     nome_aluno = input("\nDigite o nome do aluno: ")
 
     nota1_aluno = float(input("\nDigite a primeira nota do aluno: "))
@@ -35,8 +45,12 @@ def adicionar_aluno(lista_alunos):
     return lista_alunos
 
 #Função para não permitir inserir IDs iguais na lista de alunos
-def verificar_ID_aluno():
-    pass
+def verificar_ID_aluno(lista_alunos, ID_aluno):
+    for aluno in lista_alunos:
+        if aluno[0] == ID_aluno:
+            return False
+
+    return True  
 
 def visualizar_aluno(lista_alunos):
     os.system('cls')
@@ -119,7 +133,127 @@ def visualizar_todos_alunos(lista_alunos):
         os.system('pause')
 
 def calcular_media_aluno(lista_alunos):
-    pass
+    os.system('cls')
+
+    print("Opção Escolhida: Calcular a média de um aluno\n")
+
+    if len(lista_alunos) == 0:
+        print("Nenhum aluno cadastrado no sistema.\n")
+
+        print("Voltando ao menu principal.")
+        os.system('pause')
+
+        return
+
+    else:
+        try:
+            ID_aluno_busca = input("Digite o ID do aluno que deseja calcular a média: ")
+
+        except ValueError:
+            os.system('cls')
+
+            print("[ERRO]Valor inválido!")
+            print("Voltando ao menu principal.\n")
+
+            os.system('pause')
+
+        else:
+
+            if ID_aluno_busca not in [aluno[0] for aluno in lista_alunos]:
+                os.system('cls')
+
+                print(f"[ERRO]Aluno de ID {ID_aluno_busca} não encontrado no sistema.\n")
+
+                print("Voltando ao menu principal.")
+                os.system('pause')
+
+            else:
+                for aluno in lista_alunos:
+                    if aluno[0] == ID_aluno_busca:
+                        media_aluno = (aluno[2] + aluno[3]) / 2
+
+                        os.system('cls')
+
+                        print(f"Aluno: {aluno[1]} (ID: {aluno[0]})\n")
+
+                        print(f"Nota 1: {aluno[2]:.2f}")
+                        print(f"Nota 2: {aluno[3]:.2f}\n")
+
+                        print(f"Média do Aluno: {media_aluno:.2f}\n")
+
+                        print("Voltando ao menu principal.")
+                        os.system('pause')
+
+def remover_aluno(lista_alunos):
+    os.system('cls')
+
+    print("Opção Escolhida: Remover um aluno\n")
+
+    if len(lista_alunos) == 0:
+        print("Nenhum aluno cadastrado no sistema.\n")
+
+        print("Voltando ao menu principal.")
+        os.system('pause')
+
+        return
+
+    else:
+        try:
+            ID_aluno_busca = input("Digite o ID do aluno que deseja remover: ")
+
+        except ValueError:
+            os.system('cls')
+
+            print("[ERRO]Valor inválido!")
+            print("Voltando ao menu principal.\n")
+
+            os.system('pause')
+
+        else:
+
+            if ID_aluno_busca not in [aluno[0] for aluno in lista_alunos]:
+                os.system('cls')
+
+                print(f"[ERRO]Aluno de ID {ID_aluno_busca} não encontrado no sistema.\n")
+
+                print("Voltando ao menu principal.")
+                os.system('pause')
+
+            else:
+                os.system('cls')
+
+                print("Atenção: Esta ação é irreversível!")
+                print("Dados do aluno que será removido:\n")
+
+                for aluno in lista_alunos:
+                    if aluno[0] == ID_aluno_busca:
+                        print(f"ID: {aluno[0]}")
+                        print(f"Nome: {aluno[1]}")
+                        print(f"Nota 1: {aluno[2]}")
+                        print(f"Nota 2: {aluno[3]}\n")
+
+                confirmacao = input("Tem certeza que deseja remover este aluno? (S/N): ").lower()
+
+                if confirmacao == 's':
+                    os.system('cls')
+
+                    index_aluno = encontrar_indice_aluno(lista_alunos, ID_aluno_busca)
+
+                    lista_alunos.remove(lista_alunos[index_aluno])
+
+                    print(f"Aluno de ID {ID_aluno_busca} removido com sucesso!\n")
+
+                    print("Voltando ao menu principal.")
+                    os.system('pause')
+
+                    return lista_alunos
+
+#Função para encontrar o index do aluno na lista
+def encontrar_indice_aluno(lista_alunos, ID_aluno_busca):
+    for aluno in lista_alunos:
+        if aluno[0] == ID_aluno_busca:
+            return lista_alunos.index(aluno)
+
 
 while continuidade_menu == True:
     os.system('cls')
@@ -164,7 +298,7 @@ while continuidade_menu == True:
                 calcular_media_aluno(lista_alunos)
 
             case 5:
-                pass
+                remover_aluno(lista_alunos)
 
             case 6:
                 os.system('cls')
@@ -183,4 +317,3 @@ while continuidade_menu == True:
                 print("Voltando ao menu.\n")
 
                 os.system('pause')
-
